0374dcee66d0b1ba0f18af2ba15d8a6ae272aaf3
ericlovesmath <ericlovesmath@gmail.com>
2021-03-11 00:50:16 -0800

    Bash scripts, wall_cell w/ class

--- Python/Wall_Cell.py
+++ Python/Wall_Cell.py
@@ -1,39 +1,50 @@
-from math import cos, sin, pi, sqrt
+from math import cos, sin, pi, sqrt, hypot
 import os
 
-folder_name = "fila_256"
+FOLDER_NAME = "fila_256"
 try:
-    os.mkdir(folder_name)
+    os.mkdir(FOLDER_NAME)
 except OSError:
-    print(f"Directory {folder_name} already exists")
+    print(f"Directory {FOLDER_NAME} already exists")
 else:
-    print(f"Created directory {folder_name}")
+    print(f"Created directory {FOLDER_NAME}")
 
-# Decimal to Scientific Notation
 def b10(num):
+    """Decimal to Scientific Notation"""
     return "{:.10e}".format(num)
 
-class Cells:
-    def __init__(self):
-        pass
+def dist(p1, p2):
+    return hypot(p2[0]-p1[0], p2[1]-p1[1])
 
-# Generates coordinates of circle
-class Circle(Cells):
+class Circle():
+    """Generates coordinates of circle"""
     def __init__(self, x, y, r, n):
         self.x = x
         self.y = y
         self.r = r
         self.n = n
-        self.coords = [(cos(2*pi/n_circ*i)*r + x,
-                        sin(2*pi/n_circ*i)*r + y)
-                        for i in range(0,n_circ)]
+        self.coords = [(cos(2*pi/N_CELL*i)*r + x,
+                        sin(2*pi/N_CELL*i)*r + y)
+                        for i in range(0,N_CELL)]
+        self.vert_count = len(self.coords)
+        self.spring_count = len(self.coords)
+        self.beam_count = len(self.coords)
     def rotate(self, angle):
-        self.coords = [(cos(2*pi/n_circ*i + angle)*r + x,
-                        sin(2*pi/n_circ*i+ angle)*r + y)
-                        for i in range(0,n_circ)]
+        self.coords = [(cos(2*pi/N_CELL*i + angle)*r + x,
+                        sin(2*pi/N_CELL*i+ angle)*r + y)
+                        for i in range(0,N_CELL)]
+    def gen_spring(self, offset=0):
+        return [f"{(x)%self.vert_count + offset} {(x+1)%self.vert_count + offset} "\
+                f"{b10(SPRING_CONSTANT)} {b10(REST_LENGTH)}\n"
+                for x in range(self.spring_count)]
+    def gen_beam(self, offset=0):
+        return [f"{(x)%self.vert_count+offset} {(x+1)%self.vert_count+offset} "\
+                f"{(x+2)%self.vert_count+offset} "\
+                f"{b10(RIGIDITY)}\n"
+                for x in range(self.vert_count)]
 
-# Generates coordinates of a wall
-class Wall(Cells):
+class Wall():
+    """Generates coordinates of a wall"""
     def __init__(self, x1, y1, x2, y2, n):
         self.x1 = x1
         self.x2 = x2
@@ -42,66 +53,86 @@ class Wall(Cells):
         self.n = n
         self.coords = [(x1 + i*(x2-x1)/n, y1 + i*(y2-y1)/n)
             for i in range(0,n+1)]
+        self.vert_count = len(self.coords)
+        self.spring_count = len(self.coords) - 1
+        self.beam_count = len(self.coords) - 2
+    def gen_spring(self, offset=0):
+        return [f"{x+offset} {x+offset+1} {b10(SPRING_CONSTANT)} "\
+                f"{b10(dist(self.coords[x], self.coords[x+1]))}\n"
+                for x in range(self.spring_count)]
+    def gen_beam(self, offset=0):
+        return [f"{x+offset} {x+1+offset} {x+2+offset} "\
+                f"{b10(RIGIDITY)}\n"
+                for x in range(self.beam_count)]
 
 # Variables
 r, (x, y) = 2, (6, 4)                       # Circle 1, Radius and Center
 x1, y1, x2, y2 = 2,1, 2,7
-n_circ = 64                                           # Number of Points
-n_wall = 64                                           # Number of Points
-spring_const = 5000                              # Spring Constant
-rest_length = round(r*sqrt(2-2*cos(2*pi/n_circ)), 3)  # Rest Length (recommended listed)
-Rigidity = 4000000                               # Beam Rigidity
+N_CELL = 64                                           # Number of Points
+N_WALL = 64                                           # Number of Points
+SPRING_CONSTANT = 5000                              # Spring Constant
+REST_LENGTH = round(r*sqrt(2-2*cos(2*pi/N_CELL)), 3)  # Rest Length (recommended listed)
+RIGIDITY = 4000000                               # Beam RIGIDITY
 
 ##########################################################################################
 #################################### Vertex Formation ####################################
 ##########################################################################################
 
-file_out = open(f"./{folder_name}/fila_256.vertex", "w")
+# x-coord, y-coord
 
-cell_wall = Wall(x1, y1, x2, y2, n_wall).coords
+file_out = open(f"./{FOLDER_NAME}/fila_256.vertex", "w")
 
-target_cell = Circle(x,y,r,n_circ).coords
+cell_wall = Wall(x1, y1, x2, y2, N_WALL)
 
-file_out.write(f"{n_wall + n_circ + 1}\n")
-for point in cell_wall + target_cell:
+target_cell = Circle(x,y,r,N_CELL)
+
+verts = cell_wall.vert_count + target_cell.vert_count
+file_out.write(f"{verts}\n")
+
+for point in cell_wall.coords + target_cell.coords:
     file_out.write(f"{b10(point[0])} {b10(point[1])}\n")
 
-file_out.write("# x-coord, y-coord\n")
-
-print(f"Success: ~/{folder_name}/fila_256.vertex")
+print(f"Success: ~/{FOLDER_NAME}/fila_256.vertex")
 file_out.close()
 
 ##########################################################################################
 #################################### Spring Formation ####################################
 ##########################################################################################
 
-file_out = open(f"./{folder_name}/fila_256.spring", "w")
+# idx1, idx2, spring constant, rest length
 
-file_out.write(f"{n_circ + n_wall} # INACCURATE number of springs in file\n")
+file_out = open(f"./{FOLDER_NAME}/fila_256.spring", "w")
 
-for x in range(n_wall):
-    file_out.write(f"{x} {x+1} {b10(spring_const)} {b10((y2-y1)/n_wall)}\n") #This will bite me later
+springs = cell_wall.spring_count + target_cell.spring_count
 
-for x in range(n_circ):
-    file_out.write(f"{n_wall+1 + (x%n_circ)} {n_wall+1 + (x+1)%n_circ} {b10(spring_const)} {b10(rest_length)}\n")
+file_out.write(f"{springs} # number of springs in file\n")
 
-file_out.write("# idx1, idx2, spring constant, rest length\n")
+for spring in cell_wall.gen_spring():
+    file_out.write(spring)
+
+for spring in target_cell.gen_spring(cell_wall.vert_count):
+    file_out.write(spring)
+
 file_out.close()
 
-print(f"Success: ~/{folder_name}/fila_256.spring")
+print(f"Success: ~/{FOLDER_NAME}/fila_256.spring")
 
 ##########################################################################################
 ##################################### Beam Formation #####################################
 ##########################################################################################
 
-file_out = open(f"./{folder_name}/fila_256.beam", "w")
+# idx1, idx2, idx3, rigidity
+file_out = open(f"./{FOLDER_NAME}/fila_256.beam", "w")
 
-file_out.write(f"{128} # INACCURATE number of beams in file\n")
+file_out.write(f"{cell_wall.beam_count + target_cell.beam_count}"\
+               "# INACCURATE number of beams in file\n")
 
-for x in range(n_wall+n_circ+1):
-    file_out.write(f"{x%(n_wall+n_circ+1)} {(x+1)%(n_wall+n_circ+1)} {(x+2)%(n_wall+n_circ+1)} {b10(Rigidity)}\n")
+for beam in cell_wall.gen_beam():
+    file_out.write(beam)
 
-file_out.write("# idx1, idx2, idx3, rigidity\n")
+for beam in target_cell.gen_beam(cell_wall.vert_count):
+    file_out.write(beam)
+
 file_out.close()
 
-print(f"Success: ~/{folder_name}/fila_256.beam")
+print(f"Success: ~/{FOLDER_NAME}/fila_256.beam")
