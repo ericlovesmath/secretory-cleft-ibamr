a5c1229b96800755996c0295326eca9d7ef0bed0
ericlovesmath <ericlovesmath@gmail.com>
2021-03-25 13:58:21 -0700

    Update sims with class

--- Python/Circle_old.py
+++ Python/Circle_old.py
@@ -1,39 +1,53 @@
 import math
 import os
 
-#folder_name = input("Output Folder Name: ")
+# folder_name = input("Output Folder Name: ")
 folder_name = "3"
 
-try: # Creates folder named folder_name
+try:  # Creates folder named folder_name
     os.mkdir(folder_name)
 except OSError:
-    print ("\nCreation of the directory %s failed / Already Exists\n" % folder_name)
+    print(
+        "\nCreation of the directory %s failed / Already Exists\n"
+        % folder_name
+    )
 else:
-    print ("\nSuccessfully created the directory %s \n" % folder_name)
+    print("\nSuccessfully created the directory %s \n" % folder_name)
 
-r1 = 1 # radius 1
-x1 = 2 # x coordinate of center 1
-y1 = 2 # y coordinate of center 1
+r1 = 1  # radius 1
+x1 = 2  # x coordinate of center 1
+y1 = 2  # y coordinate of center 1
 
-r2 = 1 # radius 2
-x2 = 5 # x coordinate of center 2
-y2 = 2 # y coordinate of center 2
+r2 = 1  # radius 2
+x2 = 5  # x coordinate of center 2
+y2 = 2  # y coordinate of center 2
 
-n = 32 # number of points
+n = 32  # number of points
 
-def tenDig(num): # Turns numbers into the 0.0000000000+e0 format
+
+def tenDig(num):  # Turns numbers into the 0.0000000000+e0 format
     return "{:.10e}".format(num)
 
-def genCircle(x,y,r,n):
-    return [(math.cos(2*math.pi/n*i)*r + x,math.sin(2*math.pi/n*i)*r + y) for i in range(0,n)]
 
-fout = open ("./" + folder_name + "/fila_256.vertex", 'w')
+def genCircle(x, y, r, n):
+    return [
+        (
+            math.cos(2 * math.pi / n * i) * r + x,
+            math.sin(2 * math.pi / n * i) * r + y,
+        )
+        for i in range(0, n)
+    ]
 
-fout.write(str(n*2) + " "*33 + "# number of vertices in file\n") # Number of vertices in file
+
+fout = open("./" + folder_name + "/fila_256.vertex", "w")
+
+fout.write(
+    str(n * 2) + " " * 33 + "# number of vertices in file\n"
+)  # Number of vertices in file
 
 # Gets coordinates of points around Circle 1 and Circle 2
-Circle1 = genCircle(x1,y1,r1,n)
-Circle2 = genCircle(x2,y2,r2,n)
+Circle1 = genCircle(x1, y1, r1, n)
+Circle2 = genCircle(x2, y2, r2, n)
 
 for point in Circle1:
     fout.write(f"{tenDig(point[0])} {tenDig(point[1])}\n")
@@ -47,27 +61,35 @@ fout.close()
 
 # Creating fila_256.spring
 
-spring_constant = 5 # spring constant
+spring_constant = 5  # spring constant
 
-current_length1 = r1 * math.sqrt(2 - 2*math.cos(2*math.pi/n))
-current_length2 = r2 * math.sqrt(2 - 2*math.cos(2*math.pi/n))
+current_length1 = r1 * math.sqrt(2 - 2 * math.cos(2 * math.pi / n))
+current_length2 = r2 * math.sqrt(2 - 2 * math.cos(2 * math.pi / n))
 print(f"Adjacent points are {current_length1} apart (1)")
 print(f"Adjacent points are {current_length2} apart (2)")
 
-rest_length = round(current_length1, 1) # Rest Length (recommended above)
+rest_length = round(current_length1, 1)  # Rest Length (recommended above)
 
-fout = open ("./" + folder_name + "/fila_256.spring", 'w')
+fout = open("./" + folder_name + "/fila_256.spring", "w")
 
-fout.write(str(2*n + n//2 + 1) + " "*37 + "# number of springs in file\n")
+fout.write(
+    str(2 * n + n // 2 + 1) + " " * 37 + "# number of springs in file\n"
+)
 
 for x in range(n):
-    fout.write(f"{x%n} {(x+1)%n} {tenDig(spring_constant)} {tenDig(rest_length)}\n")
+    fout.write(
+        f"{x%n} {(x+1)%n} {tenDig(spring_constant)} {tenDig(rest_length)}\n"
+    )
 
 for x in range(n):
-    fout.write(f"{n + (x%n)} {n + (x+1)%n} {tenDig(spring_constant)} {tenDig(rest_length)}\n")
+    fout.write(
+        f"{n + (x%n)} {n + (x+1)%n} {tenDig(spring_constant)} {tenDig(rest_length)}\n"
+    )
 
-for x in range(-n//4, n//4 + 1):
-    fout.write(f"{x%n} {3*n//2 - x} {tenDig(spring_constant)} {tenDig(rest_length)}\n")
+for x in range(-n // 4, n // 4 + 1):
+    fout.write(
+        f"{x%n} {3*n//2 - x} {tenDig(spring_constant)} {tenDig(rest_length)}\n"
+    )
 
 fout.write("# idx1, idx2, spring constant, rest length\n")
 fout.close()
@@ -76,7 +98,7 @@ print("\nfila_256.spring created in %s" % folder_name)
 
 # Creating fila_256.beam
 
-fout = open ("./" + folder_name + "/fila_256.beam", 'w')
+fout = open("./" + folder_name + "/fila_256.beam", "w")
 
 fout.write(f"{2*n} \n")
 
@@ -92,30 +114,31 @@ fout.close()
 
 ###
 # Generates small circles centered at pi/4 and -pi/4 radians
-#smallcirc_1 = circ(circ_1[n//8][0],circ_1[n//8][1],r_small,n_small)
-#smallcirc_2 = circ(circ_1[7*n//8][0],circ_1[7*n//8][1],r_small,n_small)
+# smallcirc_1 = circ(circ_1[n//8][0],circ_1[n//8][1],r_small,n_small)
+# smallcirc_2 = circ(circ_1[7*n//8][0],circ_1[7*n//8][1],r_small,n_small)
 #
 # Cuts small circles inside larger circle
-#newSmallcirc_1 = smallcirc_1[(7*n_small//8):] + smallcirc_1[:(3*n_small//8 + 1)]
-#newSmallcirc_2 = smallcirc_2[(5*n_small//8):] + smallcirc_2[:(n_small//8 + 1)]
+# newSmallcirc_1 = smallcirc_1[(7*n_small//8):]
+# + smallcirc_1[:(3*n_small//8 + 1)]
+# newSmallcirc_2 = smallcirc_2[(5*n_small//8):] + smallcirc_2[:(n_small//8 + 1)]
 #
 # Replace overlap with cut small circle
-#circ_1[(13*n//16):(15*n//16 + 1)] = newSmallcirc_2
-#circ_1[(1*n//16):(3*n//16 + 1)] = newSmallcirc_1
+# circ_1[(13*n//16):(15*n//16 + 1)] = newSmallcirc_2
+# circ_1[(1*n//16):(3*n//16 + 1)] = newSmallcirc_1
 ###
 
 ###
 # Generates sm circles centered at pi/4 and -pi/4 radians
-#sm_start = n//8
-#sm_half  = n//2
-#sm_circ_1 = circ(circ_1[ sm_start][0], circ_1[ sm_start][1], r_sm, n_sm)
-#sm_circ_2 = circ(circ_1[-sm_start][0], circ_1[-sm_start][1], r_sm, n_sm)
+# sm_start = n//8
+# sm_half  = n//2
+# sm_circ_1 = circ(circ_1[ sm_start][0], circ_1[ sm_start][1], r_sm, n_sm)
+# sm_circ_2 = circ(circ_1[-sm_start][0], circ_1[-sm_start][1], r_sm, n_sm)
 #
 # Cuts sm circles inside larger circle
-#new_sm_circ_1 = sm_circ_1[-sm_start:]          + sm_circ_1[:sm_half - sm_start]
-#new_sm_circ_2 = sm_circ_2[sm_half + sm_start:] + sm_circ_2[:sm_start + 1]
+# new_sm_circ_1 = sm_circ_1[-sm_start:]          + sm_circ_1[:sm_half - sm_start]
+# new_sm_circ_2 = sm_circ_2[sm_half + sm_start:] + sm_circ_2[:sm_start + 1]
 #
-#Replace overlap with cut sm circle
-#circ_1[(13*n//16):(15*n//16 + 1)] = new_sm_circ_2
-#circ_1[(1*n//16):(3*n//16 + 1)] = new_sm_circ_1
+# Replace overlap with cut sm circle
+# circ_1[(13*n//16):(15*n//16 + 1)] = new_sm_circ_2
+# circ_1[(1*n//16):(3*n//16 + 1)] = new_sm_circ_1
 ###
--- Python/Wall_Cell.py
+++ Python/Wall_Cell.py
@@ -1,5 +1,5 @@
-from math import cos, sin, pi, sqrt, hypot
 import os
+from math import cos, hypot, pi, sin, sqrt
 
 FOLDER_NAME = "fila_256"
 try:
@@ -9,74 +9,106 @@ except OSError:
 else:
     print(f"Created directory {FOLDER_NAME}")
 
+
 def b10(num):
-    """Decimal to Scientific Notation"""
+    """Decimal to Scientific Notation."""
     return "{:.10e}".format(num)
 
-def dist(p1, p2):
-    return hypot(p2[0]-p1[0], p2[1]-p1[1])
 
-class Circle():
-    """Generates coordinates of circle"""
+def dist(p1, p2):
+    """Distance between two points."""
+    return hypot(p2[0] - p1[0], p2[1] - p1[1])
+
+
+class Circle:
+    """Generates coordinates of circle."""
+
     def __init__(self, x, y, r, n):
         self.x = x
         self.y = y
         self.r = r
         self.n = n
-        self.coords = [(cos(2*pi/N_CELL*i)*r + x,
-                        sin(2*pi/N_CELL*i)*r + y)
-                        for i in range(0,N_CELL)]
+        self.coords = [
+            (
+                cos(2 * pi / N_CELL * i) * r + x,
+                sin(2 * pi / N_CELL * i) * r + y,
+            )
+            for i in range(0, N_CELL)
+        ]
         self.vert_count = len(self.coords)
         self.spring_count = len(self.coords)
         self.beam_count = len(self.coords)
-    def rotate(self, angle):
-        self.coords = [(cos(2*pi/N_CELL*i + angle)*r + x,
-                        sin(2*pi/N_CELL*i+ angle)*r + y)
-                        for i in range(0,N_CELL)]
-    def gen_spring(self, offset=0):
-        return [f"{(x)%self.vert_count + offset} {(x+1)%self.vert_count + offset} "\
-                f"{b10(SPRING_CONSTANT)} {b10(REST_LENGTH)}\n"
-                for x in range(self.spring_count)]
-    def gen_beam(self, offset=0):
-        return [f"{(x)%self.vert_count+offset} {(x+1)%self.vert_count+offset} "\
-                f"{(x+2)%self.vert_count+offset} "\
-                f"{b10(RIGIDITY)}\n"
-                for x in range(self.vert_count)]
 
-class Wall():
+    def rotate(self, angle):
+        self.coords = [
+            (
+                cos(2 * pi / N_CELL * i + angle) * r + x,
+                sin(2 * pi / N_CELL * i + angle) * r + y,
+            )
+            for i in range(0, N_CELL)
+        ]
+
+    def gen_spring(self, offset=0):
+        return [
+            f"{(x)%self.vert_count + offset} "
+            f"{(x+1)%self.vert_count + offset} "
+            f"{b10(SPRING_CONSTANT)} {b10(REST_LENGTH)}\n"
+            for x in range(self.spring_count)
+        ]
+
+    def gen_beam(self, offset=0):
+        return [
+            f"{(x)%self.vert_count+offset} {(x+1)%self.vert_count+offset} "
+            f"{(x+2)%self.vert_count+offset} "
+            f"{b10(RIGIDITY)}\n"
+            for x in range(self.vert_count)
+        ]
+
+
+class Wall:
     """Generates coordinates of a wall"""
+
     def __init__(self, x1, y1, x2, y2, n):
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
         self.n = n
-        self.coords = [(x1 + i*(x2-x1)/n, y1 + i*(y2-y1)/n)
-            for i in range(0,n+1)]
+        self.coords = [
+            (x1 + i * (x2 - x1) / n, y1 + i * (y2 - y1) / n)
+            for i in range(0, n + 1)
+        ]
         self.vert_count = len(self.coords)
         self.spring_count = len(self.coords) - 1
         self.beam_count = len(self.coords) - 2
+
     def gen_spring(self, offset=0):
-        return [f"{x+offset} {x+offset+1} {b10(SPRING_CONSTANT)} "\
-                f"{b10(dist(self.coords[x], self.coords[x+1]))}\n"
-                for x in range(self.spring_count)]
+        return [
+            f"{x+offset} {x+offset+1} {b10(SPRING_CONSTANT)} "
+            f"{b10(dist(self.coords[x], self.coords[x+1]))}\n"
+            for x in range(self.spring_count)
+        ]
+
     def gen_beam(self, offset=0):
-        return [f"{x+offset} {x+1+offset} {x+2+offset} "\
-                f"{b10(RIGIDITY)}\n"
-                for x in range(self.beam_count)]
+        return [
+            f"{x+offset} {x+1+offset} {x+2+offset} " f"{b10(RIGIDITY)}\n"
+            for x in range(self.beam_count)
+        ]
+
 
 # Variables
-r, (x, y) = 2, (6, 4)                       # Circle 1, Radius and Center
-x1, y1, x2, y2 = 2,1, 2,7
-N_CELL = 32                                           # Number of Points
-N_WALL = 32                                           # Number of Points
-SPRING_CONSTANT = 200                              # Spring Constant
-REST_LENGTH = round(r*sqrt(2-2*cos(2*pi/N_CELL)), 3)  # Rest Length (recommended listed)
-RIGIDITY = 4000000                               # Beam RIGIDITY
+r, (x, y) = 2, (6, 4)  # Circle 1, Radius and Center
+x1, y1, x2, y2 = 2, 1, 2, 7
+N_CELL = 32  # Number of Points
+N_WALL = 32  # Number of Points
+SPRING_CONSTANT = 200  # Spring Constant
+# Rest Length (recommended listed)
+REST_LENGTH = round(r * sqrt(2 - 2 * cos(2 * pi / N_CELL)), 3)
+RIGIDITY = 4000000  # Beam RIGIDITY
 
-##########################################################################################
-#################################### Vertex Formation ####################################
-##########################################################################################
+###############################################################################
+#                             Vertex Formation                                #
+###############################################################################
 
 # x-coord, y-coord
 
@@ -84,7 +116,7 @@ file_out = open(f"./{FOLDER_NAME}/fila_256.vertex", "w")
 
 cell_wall = Wall(x1, y1, x2, y2, N_WALL)
 
-target_cell = Circle(x,y,r,N_CELL)
+target_cell = Circle(x, y, r, N_CELL)
 
 verts = cell_wall.vert_count + target_cell.vert_count
 file_out.write(f"{verts}\n")
@@ -95,9 +127,9 @@ for point in cell_wall.coords + target_cell.coords:
 print(f"Success: ~/{FOLDER_NAME}/fila_256.vertex")
 file_out.close()
 
-##########################################################################################
-#################################### Spring Formation ####################################
-##########################################################################################
+###############################################################################
+#                               Spring Formation                              #
+###############################################################################
 
 # idx1, idx2, spring constant, rest length
 
@@ -117,15 +149,17 @@ file_out.close()
 
 print(f"Success: ~/{FOLDER_NAME}/fila_256.spring")
 
-##########################################################################################
-##################################### Beam Formation #####################################
-##########################################################################################
+###############################################################################
+#                               Beam Formation                                #
+###############################################################################
 
 # idx1, idx2, idx3, rigidity
 file_out = open(f"./{FOLDER_NAME}/fila_256.beam", "w")
 
-file_out.write(f"{cell_wall.beam_count + target_cell.beam_count}"\
-               "# INACCURATE number of beams in file\n")
+file_out.write(
+    f"{cell_wall.beam_count + target_cell.beam_count}"
+    "# INACCURATE number of beams in file\n"
+)
 
 for beam in cell_wall.gen_beam():
     file_out.write(beam)
